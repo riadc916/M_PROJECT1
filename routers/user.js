8d2f66f42619ddaa127ceba782f9e56383027d38
RIAD MAHMUD CHOWDHURY <riadmac@RIADs-MacBook-Air.local>
2025-08-06 23:24:24 +0800

    Initial Commit

--- routers/user.js
+++ routers/user.js
@@ -1,24 +1,45 @@
-const express=require("express");
-const router=express.Router();
-const User=require("../models/user.js");
-const wrapAsync=require("../utils/wrapAsync.js");
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
+//For signuo info
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs")
-})
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{
-        const {email,username,password}=req.body;
-    let newuser=new User({email,username});
-    let usersave=await User.register(newuser,password);
-    console.log(usersave);
-    req.flash("success","Welcome to Wanderlust!");
-    res.redirect("/listings");
-    }catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      let newuser = new User({ email, username });
+      let usersave = await User.register(newuser, password);
+      console.log(usersave);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
     }
-    
-}))
-module.exports=router;
+  })
+);
+
+//for login info
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success","Welcome back!");
+    res.redirect("/listings");
+  }
+);
+
+module.exports = router;
